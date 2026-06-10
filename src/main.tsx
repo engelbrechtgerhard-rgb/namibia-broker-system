7d510320eed59cd0991f480a38ba5f8a8d2df8e0
Gerhard Engelbrecht <engelbrecht.gerhard@gmail.com>
2026-06-10 19:23:51 +0200

    Fix main.tsx routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 import {
   authority,
@@ -25,7 +26,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       automaticSilentRenew={true}
       loadUserInfo={true}
     >
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
 );
