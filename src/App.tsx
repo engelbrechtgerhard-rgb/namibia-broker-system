e88adc7b483a2b4aade224559f31ab1c16073a63
Gerhard Engelbrecht <engelbrecht.gerhard@gmail.com>
2026-06-11 18:17:12 +0200

    Add OidcCallback in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ProtectedRoute from "./ProtectedRoute";
 import Dashboard from "@/pages/Dashboard";
 import LogoutCleanup from "@/LogoutCleanup";
 import AppLayout from "@/layout/AppLayout";
+import OidcCallback from "@/pages/auth/OidcCallback";
 import Clients from "@/pages/clients/Clients";
 import ClientProfile from "@/pages/clients/ClientProfile";
 import AddClient from "@/pages/clients/AddClient";
@@ -13,82 +14,34 @@ import Reports from "@/pages/reports/Reports";
 
 export default function App() {
   return (
-    <AppLayout>
-      <Routes>
-        <Route path="/clear" element={<LogoutCleanup />} />
+    <Routes>
+      {/* Callback must be completely isolated */}
+      <Route path="/callback" element={<OidcCallback />} />
+      <Route path="/callback/" element={<OidcCallback />} />
 
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+      {/* Logout cleanup also must be isolated */}
+      <Route path="/clear" element={<LogoutCleanup />} />
 
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+      {/* Everything else goes inside the layout */}
+      <Route
+        path="/*"
+        element={
+          <AppLayout>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
 
-        <Route
-          path="/clients"
-          element={
-            <ProtectedRoute>
-              <Clients />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/clients/:clientId"
-          element={
-            <ProtectedRoute>
-              <ClientProfile />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/clients/add"
-          element={
-            <ProtectedRoute>
-              <AddClient />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/policies"
-          element={
-            <ProtectedRoute>
-              <Policies />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/claims"
-          element={
-            <ProtectedRoute>
-              <Claims />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/reports"
-          element={
-            <ProtectedRoute>
-              <Reports />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </AppLayout>
+              {/* ... all other protected routes ... */}
+            </Routes>
+          </AppLayout>
+        }
+      />
+    </Routes>
   );
 }
